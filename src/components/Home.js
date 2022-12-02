75b9faae222e41decdb8589a489bfbccbf71f767
Yasir Khan <khanyaser007@gmail.com>
2022-12-02 12:40:24 +0400

    Create preview component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Preview from './Preview';
 
 function Home() {
   return (
     <>
       <Header />
-      <div>This is the Home</div>
+      <Preview />
     </>
   );
 }
