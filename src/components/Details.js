0943f74b604a96165a165f5a3bfa92bfca7b3302
Yasir Khan <khanyaser007@gmail.com>
2022-12-02 14:07:14 +0400

    Add stats to details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,6 +6,7 @@ import { IconContext } from 'react-icons';
 import { BsHouseFill } from 'react-icons/bs';
 import './stylesheets/Details.scss';
 import pokeball from '../assets/pokeball.png';
+// import Capitalize from './utils/Capitalize';
 
 function Details() {
   const [img, setImg] = useState(pokeball);
@@ -23,15 +24,72 @@ function Details() {
   }, []);
 
   return (
-    <div>
-      <NavLink to="/">
-        <IconContext.Provider value={{ className: 'homeBtn' }}>
-          <BsHouseFill />
-        </IconContext.Provider>
-      </NavLink>
-      <h1>{params.id}</h1>
-      <img src={img} alt="" />
-    </div>
+    <>
+      <div className="detailsContainer">
+        <NavLink to="/">
+          <IconContext.Provider value={{ className: 'homeBtn' }}>
+            <BsHouseFill />
+          </IconContext.Provider>
+        </NavLink>
+        <img src={img} alt="" />
+        <h2>
+          {pokemon.name}
+        </h2>
+
+        {/* <div className="photos"> */}
+        {/* {!pokemon */}
+        {/* ? (<img src={pokemon.sprites.front_default} alt="" />) : (<img src={img} alt="" />)} */}
+        {/* <img src={pokemon.sprites.back_default} alt="" />
+          <img src={pokemon.sprites.front_shiny} alt="" />
+          <img src={pokemon.sprites.back_shiny} alt="" /> */}
+        {/* </div> */}
+      </div>
+      <div className="statsContainer">
+        <table>
+          <tr>
+            <td className="property">
+              ID:
+            </td>
+            <td className="info">
+              {pokemon.id}
+            </td>
+          </tr>
+          <tr>
+            <td className="property">
+              Name:
+            </td>
+            <td className="info">
+              {pokemon.name}
+            </td>
+          </tr>
+          <tr>
+            <td className="property">
+              Base Experience:
+            </td>
+            <td className="info">
+              {pokemon.base_experience}
+            </td>
+          </tr>
+          <tr>
+            <td className="property">
+              Height:
+            </td>
+            <td className="info">
+              {pokemon.height}
+            </td>
+          </tr>
+          <tr>
+            <td className="property">
+              Weight:
+            </td>
+            <td className="info">
+              {pokemon.weight}
+            </td>
+          </tr>
+
+        </table>
+      </div>
+    </>
   );
 }
 
